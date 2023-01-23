4361801a982ad694c77f4c54405dd77851b60afb
Thiago Vilela <thiago.vilela@outlook.com.br>
2023-01-23 15:37:34 -0300

    Create Repetition Commands - Exercise 01.py

---
+++ 04 Exercises - Repetition Commands/Repetition Commands - Exercise 01.py
@@ -0,0 +1,5 @@
+# Lista de Exercícios em Linguagem Python - Comandos de Repetição (UFU-FACOM)
+# Faça um programa que determine o mostre os cinco primeiros múltiplos de 3, considerando números maiores que 0.
+
+for i in range(1, 6):
+    print(i * 3)
